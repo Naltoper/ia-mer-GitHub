3a9a3b31ba6619291ddf25290beccc914a57e876
FABRETTI Florent <florent.fabretti@etu.univ-amu.fr>
2026-03-28 19:39:34 +0100

    - enregistrement des models 1er et 2nd passage - pret a recevoir des images de cc3

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 from src.config import *
 
-# Build S pour Dataset melangé, avec trueclas = None, 
+# Build S pour Dataset inconnnu, avec trueclas = None, 
 def buildSampleFromPathTEST (path):
     data_res = []
 
@@ -79,46 +79,137 @@ def generate_cc2_file(S_test, model_knn, model_svc, model_rfc, filename="roblof.
     except Exception as e:
         print(f"Erreur lors de l'écriture du fichier : {e}")
 
+def generate_cc2_file_2nd_passage(S_test, filename="roblof2.txt"):
+    """
+    Génère le fichier de résultats au format CC2 en utilisant le vote majoritaire.
+    """
+    # footer
+    ee = 0.00  
+    er = 0.17  
 
-# Sample complet
-S = buildSampleFromPath(path_Ailleurs, path_mer)
-print("Nombre d'image dans le sample S : ", len(S))
-features = ['X_grad', 'X_histoHSV', 'X_histo']
+    try:
+        with open(filename, 'w', encoding='utf-8') as f:
+            # header
+            f.write("# Florent Fabretti, Vincent Fabretti, Djibril Mimouni (Equipe roblof)\n")
+            f.write("# Vote majoritaire (Ensemble : KNN, LinearSVC, RandomForest)\n")
+            f.write("# KNN(k=7,cosine), SVC(C=0.0001), RFC(n=200,max_depth=None)\n")
+            f.write("# Concaténation (sur image 128x128): Gradients résumé, Histo HSV, Histo RGB\n")
+            
+            # Liste des images et prédictions
+            for img in S_test:
+                # On extrait juste "image.jpg" du path
+                clean_name = os.path.basename(img['name_path'])
+                pred = img['y_predicted_class']
+                
+                # Formatage : Nom +1 ou Nom -1
+                pred_str = f"+{pred}" if pred > 0 else f"{pred}"
+                
+                f.write(f"{clean_name} {pred_str}\n")
+            
+            # Footer
+            f.write(f"# EE = {ee:.2f}\n")
+            f.write(f"# ER = {er:.2f}\n")
+
+        print(f"Fichier {filename} généré avec succès.")
+        
+    except Exception as e:
+        print(f"Erreur lors de l'écriture du fichier : {e}")
 
 
-######## ENTRAINEMENT ###########################################################
-# model_naive = fitFrom(['X_grad', 'X_histoHSV'], S, NAIVE)
+# # Sample complet
+# S = buildSampleFromPath(path_Ailleurs, path_mer)
+# print("Nombre d'image dans le sample S : ", len(S))
+
+
+
+
+# ######## ENTRAINEMENT 1er passage ###########################################################
+# # Features 1er passage
+# features = ['X_grad', 'X_histoHSV', 'X_histo']
+
 # model_knn = fitFrom(features, S, KNN)
 # model_linearSVC = fitFrom(features, S, LINEAR_SVC)
 # model_rfc = fitFrom(features, S, RFC)
+# model_gb = fitFrom(features, S, GB)
+# model_SVC = fitFrom(features, S, SVC_)
+
+
+# ######## SAUVEGARDE DES MODELS de 1er passage ###################################################
+# # Une fois les modèles entraînés :
+# mes_modeles_1er = {
+#     'knn': model_knn,
+#     'linear_svc': model_linearSVC,
+#     'rfc': model_rfc,
+#     'gb' : model_gb,
+#     'svc': model_SVC
+# }
+# save_models(mes_modeles_1er, filename="mes_modeles_1er.joblib")
 
 
 
+# ######## ENTRAINEMENT 2nd passage ###########################################################
+# # On fait le premier passage pour avoir ses predictions
+# votePredict5(S, model_knn, model_linearSVC, model_rfc, model_gb, model_SVC)
+# # Features 2nd passage on ajoute les predictions du 1er passage
+# features_2nd_passage = ['X_grad', 'X_histoHSV', 'X_histo', 'y_predicted_class']
 
-######## SAUVEGARDE DES MODELS ###################################################
-# Une fois les modèles entraînés :
-mes_modeles = {
-    'knn': model_knn,
-    'svc': model_linearSVC,
-    'rfc': model_rfc
-}
-# save_models(mes_modeles)
+# model_knn_2nd = fitFrom(features_2nd_passage, S, KNN)
+# model_linearSVC_2nd = fitFrom(features_2nd_passage, S, LINEAR_SVC)
+# model_rfc_2nd = fitFrom(features_2nd_passage, S, RFC)
+# model_gb_2nd = fitFrom(features_2nd_passage, S, GB)
+# model_SVC_2nd = fitFrom(features_2nd_passage, S, SVC_)
 
 
-######## TEST CC2 #############################################################0.22
+
+# ######## SAUVEGARDE DES MODELS de 2nd passage ###################################################
+# # Une fois les modèles entraînés :
+# mes_modeles_2nd = {
+#     'knn': model_knn_2nd,
+#     'linear_svc': model_linearSVC_2nd,
+#     'rfc': model_rfc_2nd,
+#     'gb' : model_gb_2nd,
+#     'svc': model_SVC_2nd
+# }
+# save_models(mes_modeles_2nd, filename="mes_modeles_2nd.joblib")
+
+
+######## PREDICTION #############################################################0.22
 
 
 
 # Au lieu de fitFrom(), on charge directement :
-modeles_charges = load_models("models_roblof.joblib")
+# Chargement 1er passage
+modeles_charges_1er = load_models("mes_modeles_1er.joblib")
 
-if modeles_charges:
-    model_knn = modeles_charges['knn']
-    model_linearSVC = modeles_charges['svc']
-    model_rfc = modeles_charges['rfc']
+if modeles_charges_1er:
+    model_knn_1er = modeles_charges_1er['knn']
+    model_linearSVC_1er = modeles_charges_1er['linear_svc']
+    model_rfc_1er = modeles_charges_1er['rfc']
+    model_gb_1er = modeles_charges_1er['gb']
+    model_SVC_1er = modeles_charges_1er['svc']
+
+# Chargement 2nd passage
+modeles_charges_2nd = load_models("mes_modeles_2nd.joblib")
+
+if modeles_charges_2nd:
+    model_knn_2nd = modeles_charges_2nd['knn']
+    model_linearSVC_2nd = modeles_charges_2nd['linear_svc']
+    model_rfc_2nd = modeles_charges_2nd['rfc']
+    model_gb_2nd = modeles_charges_2nd['gb']
+    model_SVC_2nd = modeles_charges_2nd['svc']
 
 print("Test sur le sample inconnue...")
-S_TEST = buildSampleFromPathTEST(path_TEST)
+path_CC3 = "../../Format rendu CC3-20260323/FormatTestCC3/FormatTestCC3"
+S_TEST = buildSampleFromPathTEST(path_CC3)
+
+# 1ER PASSAGE
+votePredict5(S_TEST, model_knn_1er, model_linearSVC_1er, model_rfc_1er, model_gb_1er, model_SVC_1er)
+
+# 2ND PASSAGE
+votePredict5(S_TEST, model_knn_2nd, model_linearSVC_2nd, model_rfc_2nd, model_gb_2nd, model_SVC_2nd)
+
 
 # generer le fichier pour cc2
-generate_cc2_file(S_TEST, model_knn, model_linearSVC, model_rfc)
+# generate_cc2_file(S_TEST, model_knn, model_linearSVC, model_rfc)
+generate_cc2_file_2nd_passage(S_TEST)
+
