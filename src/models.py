8a15948e1b59472c8c372ac622af8f51acc9c97e
Fabretti Vincent <35635632+IceTong1@users.noreply.github.com>
2026-03-28 17:08:37 +0100

    Add GradientBoostingClassifier and SVC to model selection

--- src/models.py
+++ src/models.py
@@ -4,6 +4,10 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
+
+
 
 # Ici les fonctions d'entrainement, predictions des models
 scaler = StandardScaler()
@@ -17,7 +21,7 @@ def extract_X(features_to_use: list[str], S: list[dict], model, training: bool):
     data_list = []
     for img in S:
         combined_features = []
-        
+
         for feat in features_to_use:
             if feat in ['X_histo', 'X_histoHSV', 'X_grad']:
                 # Pour X_histo ou d'autres features directes (listes ou arrays) rien à faire
@@ -25,11 +29,11 @@ def extract_X(features_to_use: list[str], S: list[dict], model, training: bool):
                 combined_features.extend(array)
             else:
                 raise ValueError(f"Unsupported feature: {feat}")
-                    
+
         data_list.append(combined_features)
-        
+
     X = np.array(data_list)
-    
+
     if training:
         # Phase d'entraînement : on crée le scaler, on le sauvegarde et on l'apprend (fit)
         model.scaler = StandardScaler()
@@ -41,7 +45,7 @@ def extract_X(features_to_use: list[str], S: list[dict], model, training: bool):
         else:
             # si pas de scaler, on lever une erreur
             raise ValueError("No scaler found in model, if you test a model, it must have been trained with this fonction, with training=True")
-    
+
     return X
 
 
@@ -51,12 +55,16 @@ def chooseAlgo(algo):
     elif algo['name'] == "KNN":
         model = KNeighborsClassifier(**algo.get('hyper_param', {}))
     elif algo['name'] == "LinearSVC":
-        model = LinearSVC(**algo.get('hyper_param', {})) 
+        model = LinearSVC(**algo.get('hyper_param', {}))
     elif algo['name'] == "RFC":
-        model = RandomForestClassifier(**algo.get('hyper_param', {})) 
+        model = RandomForestClassifier(**algo.get('hyper_param', {}))
+    elif algo['name'] == "GB":
+        model = GradientBoostingClassifier(**algo.get('hyper_param', {}))
+    elif algo['name'] == "SVC":
+        model = SVC(**algo.get('hyper_param', {}))
     else:
         raise ValueError(f"Unsupported algorithm: {algo['name']}")
-    
+
     return model
 
 
@@ -68,16 +76,16 @@ def fitFrom(features_to_use: list[str], S_train: list, algo: dict):
     model = chooseAlgo(algo)
     # On creer un attribut features pour les retrouver plus tard
     model.features = features_to_use
-    
+
     # Caracteristique et cible
     X_train = extract_X(features_to_use, S_train, model, training=True) # training=True : on fit et save un scaler dans ce model
     y_train = np.array([img['y_true_class'] for img in S_train])
-    
-    
-    
+
+
+
     # Entraînement
     model.fit(X_train, y_train)
-    
+
     print(f"Entrainement terminé avec X = {features_to_use}")
     print(f"Model : {algo['name']}")
     return model
@@ -89,14 +97,14 @@ def predictFrom(S_test: list[dict], model):
     Modifie S_test en place en ajoutant/mettant à jour 'y_predicted_class'.
     """
     X_test = extract_X(model.features , S_test, model, training=False) # Fase : extract_X utilisera le scaler saved quand le model a ete entrainé
-    
+
     predictions = model.predict(X_test)
-    
+
     # Modif de S
     # Zip pour parcourir simultanément les dictionnaires et les prédictions
     for img, pred in zip(S_test, predictions):
         img['y_predicted_class'] = int(pred)
-    
+
     return S_test
 
 
@@ -104,4 +112,3 @@ def predictFrom(S_test: list[dict], model):
 
 if __name__ == '__main__':
     pass
-    
