1b563cab8700e1f226fb09cf61141239236ce6f0
Fabretti Vincent <35635632+IceTong1@users.noreply.github.com>
2026-03-28 17:10:37 +0100

    Refactor hyperparameter tuning and update models

--- src/config.py
+++ src/config.py
@@ -4,11 +4,9 @@ from src.models import *
 from src.metrics import *
 from src.votePredict import *
 from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import GradientBoostingClassifier
 import os
 
-# 
+#
 
 NAIVE = {
         "name" : "NaiveBayes",
@@ -33,19 +31,19 @@ param_grid_KNN = {
 LINEAR_SVC = {
         "name" : "LinearSVC",
         "hyper_param" : {
-            'C': 0.0001, 
-            'dual': False, 
-            'max_iter': 5000, 
+            'C': 0.0001,
+            'dual': False,
+            'max_iter': 5000,
             'penalty': 'l2'
             }
     }
 param_grid_LINEAR_SVC = {
     'C': [0.00001, 0.0001, 0.001, 0.1], # contrôle la force de la régularisation
-    'dual': [False], 
+    'dual': [False],
     'penalty': ['l2'],
     'max_iter': [5000, 10000]
 }
-   
+
 RFC = {
     "name": "RFC",
     "hyper_param": {
@@ -64,11 +62,11 @@ param_grid_RFC = {
     'min_samples_leaf': [1, 2, 5],
     'max_features': ['sqrt', 'log2']
 }
-  
+
 GB = {
     "name": "GB",
     "hyper_param": {
-        'n_estimators': 200,
+        'n_estimators': 300,
         'learning_rate': 0.1,
         'max_depth': 3,
         'min_samples_split': 2,
@@ -78,27 +76,44 @@ GB = {
     }
 }
 param_grid_GB = {
-    'n_estimators': [100, 200, 300],
-    'learning_rate': [0.01, 0.05, 0.1],
-    'max_depth': [3, 5, 7],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [1, 2, 5],
-    'max_features': ['sqrt', 'log2']
+    'n_estimators': [100, 300],
+    'learning_rate': [0.05, 0.1],
+    'max_depth': [3, 5],
+    'min_samples_split': [2, 10],
+    'max_features': ['sqrt'],
+    'subsample': [0.8]                   # accélère le fit en utilisant 80% des données par arbre
 }
-   
-   
+
+SVC_ = {
+    "name": "SVC",
+    "hyper_param": {
+        'C': 10,
+        'gamma': 'scale',
+        'kernel': 'rbf',
+        'degree': 3,
+        'class_weight': None,
+    }
+}
+param_grid_SVC = {
+    'C': [0.1, 1, 10, 100],
+    'gamma': ['scale', 'auto', 0.01],
+    'kernel': ['rbf', 'poly'],
+    'degree': [3],
+    'class_weight': [None, 'balanced']
+}
+
 def hyper_param_research(features_to_use: list[str], S: list[dict], model,param_grid: dict):
     # Avant de lancer cette fonction il faut :
     # définir la grille des paramètres à tester
     # initialiser le modèle de base
 
     # On configure la recherche (cv=5 signifie qu'il divise le set d'images en 5 morceaux)
-    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, 
-                            cv=5, scoring='accuracy', n_jobs=-1, verbose=1,
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid,
+                            cv=5, scoring='accuracy', n_jobs=-1, verbose=2,
                             return_train_score=True)
-    
+
     # On extrait X et y
-    X_train = extract_X(features_to_use, S)
+    X_train = extract_X(features_to_use, S, model, training=True)
     y_train = np.array([img['y_true_class'] for img in S])
 
     # On fit sur (X_train, y_train)
@@ -107,37 +122,13 @@ def hyper_param_research(features_to_use: list[str], S: list[dict], model,param_
     # On récupère les meilleurs paramètres
     results = grid_search.cv_results_
 
-    best_idx = -1
-    best_score_robust = -1
-    
-    # ON VEUT LES PARAM OPTIMAUX, PAS JUSTE LES MEILLEURS
-    # On parcourt tous les tests effectués par GridSearchCV
-    for i in range(len(results['params'])):
-        mean_test = results['mean_test_score'][i]
-        mean_train = results['mean_train_score'][i]
-        gap = mean_train - mean_test
-        
-        # CRITÈRE D'OPTIMALITÉ : 
-        # On veut le meilleur score de test, 
-        # MAIS on ignore les modèles où l'overfit est trop grand (> 10%)
-        if gap < 0.10: 
-            if mean_test > best_score_robust:
-                best_score_robust = mean_test
-                best_idx = i
-
-    # Si aucun modèle n'est sous les 10% d'écart, on prend quand même le meilleur par défaut
-    if best_idx == -1:
-        best_idx = grid_search.best_index_
+    best_idx = grid_search.best_index_
 
     optimal_params = results['params'][best_idx]
-    
+
     print("\n--- Paramètres Optimaux (Robustes) ---")
     print(f"Paramètres : {optimal_params}")
     print(f"Validation Score : {results['mean_test_score'][best_idx]:.2%}")
     print(f"Overfit Gap : {(results['mean_train_score'][best_idx] - results['mean_test_score'][best_idx]):.2%}")
-    
-    return optimal_params   
-    
-    
 
-
+    return optimal_params
